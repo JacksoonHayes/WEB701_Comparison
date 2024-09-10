630df6023b5cd612d47801126bb96468ac8b3317
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2024-09-10 14:57:28 +1200

    mongodb connection added, placeholder for angular created

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport');
+const config = require('./config/database');
+
+// Connect to routes
+const users = require('./routes/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const users = require('./routes/users');
+// Connect to MongoDB database
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: ' + config.database);
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 // CORS Middleware
 app.use(cors());
 // Body Parser Middleware
 app.use(bodyParser.json());
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', users);
 
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/growandgive',
+    secret: 'yoursecret'
+}
