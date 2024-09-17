63c6fde67b5fe248b3c0c255f2ddbd596a6414dd
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2024-09-18 11:03:35 +1200

    angular installed, components created, navbar finished with routing working

---
+++ angular_src/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'dashboard', component: DashboardComponent }
+];
